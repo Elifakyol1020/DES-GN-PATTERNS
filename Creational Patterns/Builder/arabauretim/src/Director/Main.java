6c4ef13062a55e2db826980804d9c68ceb3a2d57
Elif Akyol <129938208+Elifakyol1020@users.noreply.github.com>
2024-10-26 14:49:25 +0300

    Add files via upload

---
+++ Creational Patterns/Builder/arabauretim/src/Director/Main.java
@@ -0,0 +1,27 @@
+package Director;
+
+import Car.Manual;
+import Builder.CarBuilder;
+import Builder.CarManualBuilder;
+import Car.Car;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Director director = new Director();
+
+        CarBuilder builder = new CarBuilder();
+        director.constructSportsCar(builder);
+
+        Car car = builder.getResult();
+        System.out.println("Car built:\n" + car.getCarType());
+
+
+        CarManualBuilder manualBuilder = new CarManualBuilder();
+
+        director.constructSportsCar(manualBuilder);
+        Manual carManual = manualBuilder.getResult();
+        System.out.println("\nCar manual built:\n" + carManual.print());
+    }
+
+}
