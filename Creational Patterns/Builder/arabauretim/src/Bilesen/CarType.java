d9c113e47f1260211b1a7fe2eac7e2e435d4deb3
Elif Akyol <129938208+Elifakyol1020@users.noreply.github.com>
2024-10-26 14:47:53 +0300

    Delete Creational Patterns/Builder/arabauretim directory

--- Creational Patterns/Builder/arabauretim/src/Bilesen/CarType.java
+++
@@ -1,5 +0,0 @@
-package Bilesen;
-
-public enum CarType {
-    CITY_CAR, SPORTS_CAR, SUV
-}
--- Creational Patterns/Builder/arabauretim/src/Bilesen/Transmission.java
+++
@@ -1,5 +0,0 @@
-package Bilesen;
-
-public enum Transmission {
-    SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC
-}
